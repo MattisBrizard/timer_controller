474820b75f365cbff692bc6593b67db45fcfc3cb
Mattis Brizard <mattis.brizard@the-ring.io>
2022-08-24 18:40:19 +0200

    Improved pub score

--- test/timer_controller_test.dart
+++ test/timer_controller_test.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:timer_controller/src/timer_controller.dart';
 import 'package:timer_controller/timer_controller.dart';
 
 void main() {
